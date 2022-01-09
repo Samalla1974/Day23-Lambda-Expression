8116b92a9a6bc2cde4c6b2a136b555fa2625aab6
Samalla1974 <94819969+Samalla1974@users.noreply.github.com>
2022-01-09 20:41:23 +0530

    Add files via upload

---
+++ UserRegistration.java
@@ -0,0 +1,48 @@
+package com.bridgelabz.userregistration;
+
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+	private static final String FIRST_NAME_REGEX = "^[A-Z][a-z]{2,}";
+	private static final String LAST_NAME_REGEX = "^[A-Z][a-z]{2,}$";
+	private static final String EMAIL_REGEX = "^[a-zA-Z0-9_-]+(?:\\.[a-zA-Z0-9_+-]+)*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
+	private static final String PHONE_NUMBER_REGEX = "^[0-9]{2}[0-9]{10}";
+	private static final String PASSWORD_REGEX = "(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_-])[a-zA-Z0-9].{8,}$";
+	static boolean isFirstNameValid,isLastNameValid,isMailValid,isNumberValid,isPasswordValid;
+	
+	public boolean validateFirstName(String firstName) {
+		isFirstNameValid= Pattern.matches(FIRST_NAME_REGEX,firstName);
+		if(!isFirstNameValid) {
+			System.out.println("First Name is invalid");
+		}
+		return isFirstNameValid;
+	}
+	public boolean validateLastName(String lastName) {
+		isLastNameValid= Pattern.matches(LAST_NAME_REGEX, lastName);
+		if(!isLastNameValid) {
+			System.out.println("Last Name is invalid");
+		}
+		return Pattern.matches(LAST_NAME_REGEX, lastName);
+	}
+	public boolean validateEmail(String mail) {
+		isMailValid= Pattern.matches(EMAIL_REGEX, mail);
+		if(!isMailValid) {
+			System.out.println("E-mail is invalid");
+		}
+		return Pattern.matches(EMAIL_REGEX, mail);
+	}
+	public boolean validatePhoneNumber(String phoneNumber) {
+		isNumberValid= Pattern.matches(PHONE_NUMBER_REGEX, phoneNumber);
+		if(!isNumberValid) {
+			System.out.println("Phone Number is invalid");
+		}
+		return Pattern.matches(PHONE_NUMBER_REGEX, phoneNumber);
+	}
+	public boolean validatePassword(String password) {
+		isPasswordValid= Pattern.matches(PASSWORD_REGEX, password);
+		if(!isPasswordValid) {
+			System.out.println("Password is invalid");
+		}
+		return Pattern.matches(PASSWORD_REGEX, password);
+	}
+}	
---
+++ UserRegistrationMain.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.userregistration;
+import java.util.Scanner;
+
+	public class UserRegistrationMain {
+		public static void main(String[] args) {
+			UserRegistration registration = new UserRegistration();
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter First Name: ");
+			String firstName = sc.next();
+			registration.validateFirstName(firstName);
+			System.out.println("Enter Last Name: ");
+			String lastName = sc.next();
+			registration.validateLastName(lastName);
+			System.out.println("Enter Phone Number: ");
+			String number = sc.next();
+			registration.validatePhoneNumber(number);
+			System.out.println("Enter Password: ");
+			String password = sc.next();
+			registration.validatePhoneNumber(password);	
+		}
+
+	}
+
